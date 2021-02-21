a932ef57db81a2805f7a50df095eee315231f464
wataru.muya <wmuya0303@gmail.com>
2021-02-21 15:25:08 +0900

    pod installとgitignoreの作成まで実施

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 撫養航 on 2021/02/20.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var txt_title: UITextField!
+    @IBOutlet weak var txt_contents: UITextView!
+    @IBOutlet weak var date_piker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,11 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tbl_list: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tbl_list.delegate = self
+        tbl_list.dataSource = self
+    }
+    
+    // Cellの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    // Cellの内容
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能な cell を得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
+
+    // Cell選択時
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 
+        performSegue(withIdentifier: "cellSegue",sender: nil)
+    }
+
+    // セルが削除が可能なことを伝えるメソッド（？）
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
+        return .delete
+    }
+
+    // 削除ボタン押下時
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
     }
 
 
